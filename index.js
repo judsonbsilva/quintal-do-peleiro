62ed8ef8ef06e059352ab294e60ab5a544ad502e
Judson Silva <judsonbarroso@gmail.com>
2016-07-15 15:05:09 -0300

    add electron interface

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-var app = require('app');
-var BrowserWindow = require('browser-window');
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+const fs = require('fs');
 
-app.on('ready', function() {
-  var mainWindow = new BrowserWindow({
-    width: 960,
-    height: 640
-  })
-  mainWindow.loadUrl('file://' + __dirname + '/index.html')
+// adds debug features like hotkeys for triggering dev tools and reload
+require('electron-debug')();
+// prevent window being garbage collected
+let mainWindow;
+
+function onClosed() {
+    // dereference the window
+    // for multiple windows store them in an array
+    mainWindow = null;
+}
+
+function createMainWindow() {
+    const win = new electron.BrowserWindow({
+        width: 960,
+        height: 640,
+        resizable: false
+    });
+
+    win.loadURL(`file://${__dirname}/index.html`);
+    win.on('closed', onClosed);
+
+    return win;
+}
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
+
+app.on('activate', () => {
+    if (!mainWindow) {
+        mainWindow = createMainWindow();
+    }
+});
+
+app.on('ready', () => {
+    mainWindow = createMainWindow();
+});
+
